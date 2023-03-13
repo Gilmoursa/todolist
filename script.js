9ccbf5b60bbc118d15ce15b7967fea5bc64d5660
Austin <gilmoursa@gmail.com>
2023-03-12 20:34:10 -0400

    adding variables from HTML to  JS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // JS to create the todo list will go here
+
+// creating my variables
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
