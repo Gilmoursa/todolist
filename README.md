c6274ca3b78e0d24b780c0b1dba12fb49ea4b3ce
Austin <gilmoursa@gmail.com>
2023-03-12 20:36:46 -0400

    creating the JS to add and delete a task

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todolist
 a simple to do list for working with JS, CSS, and HTML
+ The goal here is just to setup a github repo and get all the parts working together
+ 
--- script.js
+++ script.js
@@ -4,3 +4,18 @@
 const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
+// creating and deleting tasks
+addButton.addEventListener("click", () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.innerHTML = `${taskText} <span>X</span>`;
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+  
+      const deleteButton = taskItem.querySelector("span");
+      deleteButton.addEventListener("click", () => {
+        taskList.removeChild(taskItem);
+      });
+    }
+  });
